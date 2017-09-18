2e7a0bc45c67e0d7eef65d8fc7b8118b929c337c
kingmolnar <peter@kingmolnar.com>
2017-09-18 11:53:59 -0400

    new

---
+++ 04-Pandas-Data-Tables/HW04/hw4_template.py
@@ -0,0 +1,41 @@
+# Solutions to homework 4
+import numpy as np
+import pandas as pd
+
+
+
+
+
+def load_employees():
+    pass ## fill int your code here
+
+
+
+def getFullName(df, empid):
+    pass ## fill int your code here
+
+    
+    
+
+    
+def isSales(jt):
+    pass ## fill int your code here
+
+
+
+def filterSales(df):
+    pass ## fill int your code here
+
+
+
+
+
+def getEmailListByState(df):
+    pass ## fill int your code here
+
+
+
+def managementCounts(df):
+    pass ## fill int your code here
+
+
