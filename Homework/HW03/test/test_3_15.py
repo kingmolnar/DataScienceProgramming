7405ef856a032365ff678aeb1dc373acb0fef8c6
Peter Molnar <pmolnar@gsu.edu>
2023-09-19 14:11:32 -0400

    fix test criterium

--- Homework/HW03/test/test_3_15.py
+++ Homework/HW03/test/test_3_15.py
@@ -3,4 +3,5 @@ import os
 def test_3_15():
     assert os.path.isfile('amazon_positive.txt'), "Missing output file."
     lines = open('amazon_positive.txt', 'r', encoding='utf-8').readlines()
-    assert len(lines) == 495, "Incorrect number of lines"
+    #  assert len(lines) == 495, "Incorrect number of lines"
+    assert len(lines) == 495 or len(lines) == 500, "Incorrect number of lines"
