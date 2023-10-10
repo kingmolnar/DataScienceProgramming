fed3e62df6661247ac8729ed5c855aaa14d863c9
Péter Molnár <pmolnar@gsu.edu>
2023-10-10 16:26:06 -0400

    fix

--- tools/grade_homeworks.sh
+++ tools/grade_homeworks.sh
@@ -21,15 +21,16 @@ CLASS_DIR=IFI8410F23
 GIT_REPO=https://github.com/kingmolnar/DataScienceProgramming.git
 GIT_DIR=DataScienceProgramming
 
-TEST_DIR[HW01]="${MAIN_DIR}/01-Intro-UNIX/HW01/test"
-TEST_DIR[HW02]="${MAIN_DIR}/Homework/HW02/test"
-TEST_DIR[HW03]="${MAIN_DIR}/Homework/HW03/test"
-TEST_DIR[HW04]="${MAIN_DIR}/Homework/HW04/test"
-TEST_DIR[HW05]="${MAIN_DIR}/Homework/HW05/test"
+TEST_DIR["HW01"]="${MAIN_DIR}/01-Intro-UNIX/HW01/test"
+TEST_DIR["HW02"]="${MAIN_DIR}/Homework/HW02/test"
+TEST_DIR["HW03"]="${MAIN_DIR}/Homework/HW03/test"
+TEST_DIR["HW04"]="${MAIN_DIR}/Homework/HW04/test"
+TEST_DIR["HW05"]="${MAIN_DIR}/Homework/HW05/test"
 
 BACKUP_DIR="${MAIN_DIR}/private/backup"
 
-T_DIR=$TEST_DIR[$ASSIGNMENT]
+T_DIR=${TEST_DIR[$ASSIGNMENT]}
+
 echo "--------------------------------------------------------------------------------"
 echo "Test: ${T_DIR}"
 echo "--------------------------------------------------------------------------------"
