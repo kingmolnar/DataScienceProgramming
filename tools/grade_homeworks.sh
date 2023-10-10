862d3033a101291d9e5d6f9d7e6cec13067b19ff
Péter Molnár <pmolnar@gsu.edu>
2023-10-10 16:53:01 -0400

    add log file

--- tools/grade_homeworks.sh
+++ tools/grade_homeworks.sh
@@ -29,6 +29,8 @@ TEST_DIR["HW04"]="${MAIN_DIR}/Homework/HW04/test"
 TEST_DIR["HW05"]="${MAIN_DIR}/Homework/HW05/test"
 
 BACKUP_DIR="${MAIN_DIR}/private/backup"
+LOG_DIR="${MAIN_DIR}/log"
+DSTAMP=`date +"%Y%m%d_%H%M%S"`
 
 T_DIR=`realpath ${TEST_DIR[$ASSIGNMENT]}`
 
@@ -43,7 +45,9 @@ do
         if [ -d ${HOME_DIR}/${ASSIGNMENT} ]
         then
             printf "${ASSIGNMENT}\t$U\tTEST START\n"
-            bash -c "cd ${HOME_DIR}/${ASSIGNMENT}; python3 -m pytest ${T_DIR}" 2>&1 | awk "{print \"$ASSIGNMENT\t$U\t\", \$0}"
+            bash -c "cd ${HOME_DIR}/${ASSIGNMENT}; python3 -m pytest ${T_DIR}" 2>&1 \
+                | awk "{print \"$ASSIGNMENT\t$U\t\", \$0}" \
+                | tee "${LOG_DIR}/grades_${ASSIGNMENT}_${DSTAMP}.log"
             printf "${ASSIGNMENT}\t$U\tTEST END\n\n"
         else
             printf "$ASSIGNMENT\t$U\tNO ASSIGNMENT\n\n"
