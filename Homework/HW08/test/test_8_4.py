69f3a9884a8a1bfce6ca9baf886308560ff6b9b8
Peter Molnar <pmolnar@gsu.edu>
2023-11-01 13:55:44 -0400

    initial commit

---
+++ Homework/HW08/test/test_8_4.py
@@ -0,0 +1,13 @@
+import os
+def test_8_4():
+    filename = 'answer_8_4.txt'
+    assert os.path.isfile(filename), f"Missing file `{filename}`"
+    lines = filter(
+        lambda s: not s.startswith('#'),
+        map(
+            lambda s: s.strip(),
+            open(filename, "r", encoding='utf-8').readlines()
+        )
+    )
+    text = '\n'.join(lines)
+    assert len(text)>0, f"No contents in answer file `{filename}`"
