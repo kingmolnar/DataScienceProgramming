92fa440e0fe9c7dc5769313e5e715e3c46afbddb
Péter Molnár <pmolnar@gsu.edu>
2023-11-14 16:39:07 -0500

    update test scripts

--- Homework/HW07/test/test_7_13.py
+++ Homework/HW07/test/test_7_13.py
@@ -2,8 +2,9 @@ import pandas as pd
 import hashlib
 
 def test_7_13():
-    result = pd.read_csv('penguins_7_13.csv')
-    row1 = result.iloc[0,:].to_list()
-    row3 = result.iloc[2,:].to_list()
-    assert hashlib.sha256(str(row1).encode('utf-8')).hexdigest() == '6beee7ad7f24bd59e5bb93d3f111c7b71c25591e6eb242db95db11ae4b6a48b4',  'First row values are incorrect'
-    assert hashlib.sha256(str(row3).encode('utf-8')).hexdigest() == '0586ffd672513034369fc9091e2240dc62db3bd0a87c5f013d36b316e5ceb3ff',  'Third row values are incorrect'
+    result = pd.read_csv('penguins_8_13.csv')
+    assert result.shape[1] == 4, "Invalid number of columns"
+#    row1 = result.iloc[0,:].to_list()
+#    row3 = result.iloc[2,:].to_list()
+#    assert hashlib.sha256(str(row1).encode('utf-8')).hexdigest() == '6beee7ad7f24bd59e5bb93d3f111c7b71c25591e6eb242db95db11ae4b6a48b4',  'First row values are incorrect'
+#    assert hashlib.sha256(str(row3).encode('utf-8')).hexdigest() == '0586ffd672513034369fc9091e2240dc62db3bd0a87c5f013d36b316e5ceb3ff',  'Third row values are incorrect'
--- Homework/HW07/test/test_7_20.py
+++ Homework/HW07/test/test_7_20.py
@@ -3,7 +3,8 @@ import hashlib
 
 def test_7_20():
     result = pd.read_csv('tips_7_20.csv')
-    row1 = result.iloc[0,:].to_list()
-    row2 = result.iloc[1,:].to_list()
-    assert hashlib.sha256(str(row1).encode('utf-8')).hexdigest() == '6aa4cac00d7f817e536f4c81ed042abd382ff650672548bc61018ae8e54051ff',  'First row values are incorrect'
-    assert hashlib.sha256(str(row2).encode('utf-8')).hexdigest() == '090da858c78829659c5de6ed2afac3c017649c2865bf49f80a497b389ad2c75d',  'Second row values are incorrect'
+    assert result.shape = (2, 1), "Invalid shape"
+    # row1 = result.iloc[0,:].to_list()
+    # row2 = result.iloc[1,:].to_list()
+    # assert hashlib.sha256(str(row1).encode('utf-8')).hexdigest() == '6aa4cac00d7f817e536f4c81ed042abd382ff650672548bc61018ae8e54051ff',  'First row values are incorrect'
+    # assert hashlib.sha256(str(row2).encode('utf-8')).hexdigest() == '090da858c78829659c5de6ed2afac3c017649c2865bf49f80a497b389ad2c75d',  'Second row values are incorrect'
