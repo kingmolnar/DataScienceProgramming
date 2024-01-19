e5d8136138042842fd7b255bc58377eb5a4c7593
Peter Molnar <pmolnar@gsu.edu>
2024-01-18 20:46:27 -0500

    initial commit

---
+++ tools/students_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+import os
+jp = os.path.join
+
+import pandas as pd
+import warnings
+
+PAWS_XLSX = '202401_21508_classlist.xlsx'
+PAWS_SKIP = 14
+DIR_NAME = jp(os.path.dirname(__file__), '..', '..')
+
+with warnings.catch_warnings(record=True):
+    warnings.simplefilter("always")
+    df = pd.read_excel(jp(DIR_NAME, PAWS_XLSX), skiprows=PAWS_SKIP)
+
+    print('\n'.join(df.Email.map(lambda s: s.split('@')[0], na_action='ignore').values))
