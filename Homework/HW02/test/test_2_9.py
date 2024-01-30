71aa52af67044f75710e4c94245bdf64ca89352d
Peter Molnar <pmolnar@gsu.edu>
2024-01-30 12:40:55 -0500

    fix data filename

--- Homework/HW02/test/test_2_9.py
+++ Homework/HW02/test/test_2_9.py
@@ -2,7 +2,8 @@ from solution_2_9 import load_data_col
 import numpy as np
 
 def test_2_9():
-    claimed_file = 'MotorInsuranceFraud_ClaimAmount.csv'
+    # claimed_file = 'MotorInsuranceFraud_ClaimAmount.csv'
+    claimed_file = '/data/kelleher/MotorInsuranceFraudClaim_ClaimAmount.csv'    
     x = load_data_col(claimed_file)
     if isinstance (x, np.ndarray):
         assert x.shape == (500,), "Invalid number of records"
