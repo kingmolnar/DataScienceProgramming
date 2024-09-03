b575a6d3fd241968eb1b693f1fbd33d680f8f277
Peter Molnar <pmolnar@gsu.edu>
2024-09-03 13:11:33 -0400

    update tools

--- tools/create_homework_submission_folders.sh
+++ tools/create_homework_submission_folders.sh
@@ -28,20 +28,25 @@ fi
 mkdir -p $DEST_DIR/$TERM_DIR
 chmod o=x $DEST_DIR/$TERM_DIR
 
-for STUDENT in `$TOOLS/students_list.py`
+for STUDENT in `$TOOLS/students_list.py --with-ta` 
 do
     echo "Student: $STUDENT"
     STUDENT_DIR="$DEST_DIR/$TERM_DIR/$STUDENT"
-    mkdir -p $STUDENT_DIR
-    for H in {00..10}
-    do
-        HW_DIR="${STUDENT_DIR}/HW${H}"
-        mkdir -p $HW_DIR
-    done
-    sudo chgrp -R $STUDENT $STUDENT_DIR
-    chmod -R o-rwx $STUDENT_DIR
-    chmod -R g+w $STUDENT_DIR
-    sudo chmod -R g+s $STUDENT_DIR
+    if [ ! -d $STUDENT_DIR ]
+    then
+       mkdir -p $STUDENT_DIR
+       for H in {00..10}
+       do
+           HW_DIR="${STUDENT_DIR}/HW${H}"
+           mkdir -p $HW_DIR
+       done
+       sudo chgrp -R $STUDENT $STUDENT_DIR
+       chmod -R o-rwx $STUDENT_DIR
+       chmod -R g+w $STUDENT_DIR
+       sudo chmod -R g+s $STUDENT_DIR
+    else
+       echo "${STUDENT_DIR} already exists."
+    fi
 done
 # ls -lR $DEST_DIR/$TERM_DIR
 echo "Done."
