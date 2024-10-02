8507530dab496f35aa8f4d0e3e6e8220123c2d06
Mark Jack <mjack6@gsu.edu>
2024-10-01 20:02:29 -0400

    fix: Fix formaulation of problem 5.12 in HW05 notebook

--- Homework/HW05/test/test_hw05_12.py
+++ Homework/HW05/test/test_hw05_12.py
@@ -8,19 +8,22 @@ import numpy as np
 class TestHW0512(unittest.TestCase):
     def test_multiply_dataframes_with_fill(self):
         data1 = {
-            'Column1': [1, np.nan, 3],
-            'Column2': [4, 5, np.nan]
+            'Column1': [1, 2, 3],
+            'Column2': [3, 2, 4],
         }
         data2 = {
-            'Column1': [np.nan, 2, 3],
-            'Column2': [1, np.nan, 6]
+            'Column1': [np.nan, 2, 2],
+            'Column3': [np.nan, 4, 2],
         }
         df1 = pd.DataFrame(data1)
         df2 = pd.DataFrame(data2)
+        
         result = multiply_dataframes_with_fill(df1, df2)
+        
         expected_data = {
-            'Column1': [1.0, 2.0, 9.0],
-            'Column2': [4.0, 5.0, 6.0]
+            'Column1': [1.0, 4.0, 6.0],
+            'Column2': [3.0, 2.0, 4.0],
+            'Column3': [1.0, 4.0, 2.0],
         }
         expected_df = pd.DataFrame(expected_data)
         pd.testing.assert_frame_equal(result, expected_df)
